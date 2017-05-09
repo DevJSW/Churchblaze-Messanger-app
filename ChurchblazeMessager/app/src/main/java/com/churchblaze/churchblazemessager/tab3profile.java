2384954d3b9e33c4bdd890d99be5d6404fd1f07b
DevJSW <johnwasike0@gmail.com>
2017-05-09 14:07:54 +0300

    pushing changes on privacy policy

--- ChurchblazeMessager/app/src/main/java/com/churchblaze/churchblazemessager/tab3profile.java
+++ ChurchblazeMessager/app/src/main/java/com/churchblaze/churchblazemessager/tab3profile.java
@@ -1,6 +1,7 @@
 package com.churchblaze.churchblazemessager;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -48,6 +49,17 @@ public class tab3profile extends Fragment {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(new Intent(getActivity(), NotificationActivity.class)));
+            }
+        });
+
+        LinearLayout openPrivacy = (LinearLayout) v.findViewById(R.id.lin_privacy);
+        openPrivacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // open from a browser https://churchblaze.com/help/privacy
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://churchblaze.com/help/privacy"));
+                startActivity(browserIntent);
             }
         });
 
